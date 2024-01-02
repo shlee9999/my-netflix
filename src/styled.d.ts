3d15b2db2ebbf58a3f1d9bdb2336b040dd7f3d18
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-01-02 13:38:10 +0900

    Initial Commit

---
+++ src/styled.d.ts
@@ -0,0 +1,16 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    red: string;
+    black: {
+      darkest: string;
+      darker: string;
+      lighter: string;
+    };
+    white: {
+      darker: string;
+      lighter: string;
+    };
+  }
+}
